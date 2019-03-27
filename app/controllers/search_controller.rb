57ef181903a6726ae671cd7e23a02758d64036ef
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-27 10:47:46 -0600

    Makes commit.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def index
     data = NrelService.new(params)
+    binding.pry
     @nrel_facade = NrelFacade.new(data)
   end
 end
--- spec/requests/nrel_service_spec.rb
+++ spec/requests/nrel_service_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 context 'Nrel API Request' do
   it 'returns expected data' do
     params = {"q"=>"80206"}
-    stations = NrelService.new(params['q']).get_stations
-    get "/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_API_KEY']}&location=#{zip_code}&radius=5&limit=15&fuel_type=ELEC,LPG&access=public&status=E"
+    NrelService.new(params['q']).get_stations
+    get "/alt-fuel-stations/v1/nearest.json?&location=#{zip_code}&radius=5&limit=15&fuel_type=ELEC,LPG&access=public&status=E"
 
     result = JSON.parse(response.body)
 
     expect(response).to be_successful
-    expect(result['']).to eq()
+    expect(result[0]['']).to eq()
   end
 end
